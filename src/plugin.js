b5f9d61ca09244b0f2723714a86f0b279b980696
Philipp Dieter <philipp.dieter@attic-media.de>
2018-10-10 01:49:43 +0200

    [WIP]

--- src/plugin.js
+++ src/plugin.js
@@ -1,19 +1,44 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const filedataStore = require('pug-contentswitch-loader/lib/filedata-store')
+
 function StyleguidePlugin(options) {
 }
 
 StyleguidePlugin.prototype.apply = function(compiler) {
 
   compiler.plugin('entryOption', function() {
-      new CopyWebpackPlugin([
-        {
-          from: __dirname + '/index.html',
-          to: 'styleguide/'
-        }
-
-      ]).apply(compiler)
+    new CopyWebpackPlugin([
+      {
+        from: __dirname + '/index.html',
+        to: 'styleguide/',
+      },
+      {
+        from: __dirname + '/styleguide.*.css',
+        to: 'styleguide/',
+        flatten: true,
+      },
+      {
+        from: __dirname + '/styleguide.*.js',
+        to: 'styleguide/',
+        flatten: true,
+      },
+    ]).apply(compiler)
   });
+
+  compiler.hooks.emit.tapAsync('StyleguidePlugin', (compilation, callback) => {
+    titledata = filedataStore.getTitles()
+    titledata = JSON.stringify(titledata)
+    compilation.assets['styleguide/styleguidefiles.json'] = {
+      source: function() {
+        return titledata
+      },
+      size: function() {
+        return titledata.length
+      }
+    }
+    callback()
+  })
 
 }
 
