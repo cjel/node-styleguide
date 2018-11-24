020a55640a6150621effec621286deadb63037a5
Philipp Dieter <philipp.dieter@attic-media.de>
2018-11-24 18:47:05 +0100

    [TASK] Add root option

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,26 +1,31 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-
-const filedataStore = require('pug-contentswitch-loader/lib/filedata-store')
+const CopyWebpackPlugin             = require('copy-webpack-plugin')
+const filedataStore                 = require('pug-contentswitch-loader/lib/filedata-store')
+const path                          = require('path')
 
 function StyleguidePlugin(options) {
+  this.options = options
+  if (!('root' in options)) {
+    this.options['root'] = 'styleguide'
+  }
 }
 
 StyleguidePlugin.prototype.apply = function(compiler) {
 
   compiler.hooks.afterPlugins.tap('StyleguidePlugin', (compilation) => {
+    //console.log(this.options['root'])
     new CopyWebpackPlugin([
       {
         from: __dirname + '/index.html',
-        to: 'styleguide/',
+        to: this.options['root'] + '/',
       },
       {
         from: __dirname + '/styleguide.*.css',
-        to: 'styleguide/',
+        to: this.options['root'] + '/',
         flatten: true,
       },
       {
         from: __dirname + '/styleguide.*.js',
-        to: 'styleguide/',
+        to: this.options['root'] + '/',
         flatten: true,
       },
     ]).apply(compiler)
@@ -28,9 +33,10 @@ StyleguidePlugin.prototype.apply = function(compiler) {
   });
 
   compiler.hooks.emit.tapAsync('StyleguidePlugin', (compilation, callback) => {
+    var pathRelative = path.relative(compiler.options.output.path, this.options['root'])
     titledata = filedataStore.getTitles()
     titledata = JSON.stringify(titledata)
-    compilation.assets['styleguide/styleguidefiles.json'] = {
+    compilation.assets[pathRelative + '/styleguidefiles.json'] = {
       source: function() {
         return titledata
       },
