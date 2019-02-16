737e26647963445218f15e7b85f0e5588dde5ce7
Philipp Dieter <philipp.dieter@attic-media.de>
2019-02-16 14:49:29 +0100

    [TASK] Improvements in design and function

--- src/scripts/styleguide.js
+++ src/scripts/styleguide.js
@@ -2,6 +2,31 @@ import Vue from 'vue'
 import styles from 'styles/styleguide.sass'
 import axios from 'axios'
 
+Vue.component('button-toggle-menu', {
+  data: function(){
+    return {
+      open: false,
+    }
+  },
+  methods: {
+    toggleMenu: function() {
+      this.$root.$refs['main-menu'].open = !this.$root.$refs['main-menu'].open;
+    }
+  },
+  template: '<a class="button-toggle-menu" v-on:click.prevent="toggleMenu"><slot></slot></a>'
+});
+
+Vue.component('main-menu', {
+  data: function(){
+    return {
+      open: true,
+    }
+  },
+  methods: {
+  },
+  template: '<div v-if="open"><slot></slot></div>'
+});
+
 Vue.component('main-menu-link', {
   data: function(){
     return {
@@ -10,12 +35,15 @@ Vue.component('main-menu-link', {
   },
   methods: {
     updateMainContent: function() {
-      this.$root.mainContentFile = this.$el.href
-      console.log(this.$root.mainContentFile)
-    },
+      this.$root.mainContentFile = this.$el.href;
+      if (window.innerWidth < 1088) {
+        this.$root.$refs['main-menu'].open = false;
+      }
+      console.log(this.$root.mainContentFile);
+    }
   },
-  template: '#main-menu-link-template',
-})
+  template: '#main-menu-link-template'
+});
 
 var app = new Vue({
   el: '#page',
